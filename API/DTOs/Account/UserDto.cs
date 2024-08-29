b61be81317f74d54af797b3618f0c3915f0d6f18
leminhhuybg <1>
2024-08-29 10:18:28 +0700

    không quan trọng

--- API/DTOs/Account/UserDto.cs
+++ API/DTOs/Account/UserDto.cs
@@ -2,8 +2,8 @@
 {
     public class UserDto
     {
+        public string UserName { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string JWT { get; set; }
     }
 }
