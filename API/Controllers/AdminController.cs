e8bc6b48620dffa84709e9079dbd9a0550afe025
leminhhuybg <1>
2024-09-18 07:02:22 +0700

    update role

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -39,6 +39,49 @@ namespace API.Controllers
             }
         }
 
+        [Authorize(Policy = "RequiredAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, string roles)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(roles))
+                {
+                    return BadRequest("Bạn phải chọn ít nhất 1 quyền ");
+                }
+
+                var selectedRoles = roles.Split(',').ToArray();
+
+                var user = await userManager.FindByNameAsync(username);
+
+                if (user == null) {
+                    return NotFound("User not found");
+                }
+
+                var userRoles = await userManager.GetRolesAsync(user);
+
+                var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+                if(!result.Succeeded)
+                {
+                    return BadRequest("Có vấn đề khi thêm quyền");
+                }
+
+                result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+                if (!result.Succeeded)
+                {
+                    return BadRequest("Có vấn đề khi xóa quyền");
+                }
+
+                return Ok(await userManager.GetRolesAsync(user));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.InnerException?.Message ?? ex.Message);
+            }
+        }
+
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("get-photos")]
         public ActionResult GetPhotosForModerators()
